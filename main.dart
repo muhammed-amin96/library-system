1ab11abfc0f72086ef69582141cec86dfe9ae663
Muhammad Amin <mohamedamin.fcih@gmail.com>
2024-12-21 19:28:00 +0200

    created addBook, addUser and displayInfo in library class

--- main.dart
+++ main.dart
@@ -17,4 +17,53 @@ class User {
   }
 }
 
-void main() {}
+class Library {
+  List<Book> books = [];
+  List<User> users = [];
+  void addBook(Book book) {
+    books.add(book);
+    print("Book: ${book.bookTitle} has been added.");
+    print("====================================================");
+  }
+
+  void addUser(User user) {
+    users.add(user);
+    print("User: ${user.userName} has been added.");
+    print("====================================================");
+  }
+
+  void displayLibraryInfo() {
+    print("Library Books:");
+    for (Book book in books) {
+      book.displayBookInfo();
+      print("====================================================");
+    }
+    print("====================================================");
+    print("Library Users:");
+    for (User user in users) {
+      user.displayUserInfo();
+      print("====================================================");
+    }
+  }
+}
+
+void main() {
+  // Create Library instance
+  Library library = Library();
+
+  // Add books
+  Book book1 = Book('B001', 'The Great Gatsby');
+  Book book2 = Book('B002', '1984');
+  Book book3 = Book('B003', 'To Kill a Mockingbird');
+  library.addBook(book1);
+  library.addBook(book2);
+  library.addBook(book3);
+
+  // Add users
+  User user1 = User('U001', 'Ahmed');
+  User user2 = User('U002', 'Esraa');
+  library.addUser(user1);
+  library.addUser(user2);
+
+  library.displayLibraryInfo();
+}
