9bb1f9b2b3b1c73bce13ff9388c404e198f05a9b
Muhammad Amin <mohamedamin.fcih@gmail.com>
2024-12-21 20:21:03 +0200

    added functionality to borrow books

--- main.dart
+++ main.dart
@@ -45,6 +45,21 @@ class Library {
       print("====================================================");
     }
   }
+
+  void borrowBook(String bookId, String userId) {
+    //search for first element in books list that matches id given by user and if not found return empty book object
+    var bookToBeBorrowed = books.firstWhere((b) => b.bookId == bookId,
+        orElse: () => Book(" ", " "));
+    if (bookToBeBorrowed.bookId == " ") {
+      print("Book not found");
+    } else if (bookToBeBorrowed.isBooked) {
+      print("Book is already borrowed.");
+    } else {
+      bookToBeBorrowed.isBooked = true;
+      print(
+          "Book: ${bookToBeBorrowed.bookTitle} borrowed by user with id: $userId");
+    }
+  }
 }
 
 void main() {
@@ -64,6 +79,12 @@ void main() {
   User user2 = User('U002', 'Esraa');
   library.addUser(user1);
   library.addUser(user2);
+  // Borrow Books
+  library.borrowBook('B001', 'U001');
+  library.borrowBook('B002', 'U002');
+  library.borrowBook('B003', 'U001');
+  library.borrowBook('B004', 'U001'); //book not found case
+  library.borrowBook('B001', 'U001'); //book already booked case
 
   library.displayLibraryInfo();
 }
